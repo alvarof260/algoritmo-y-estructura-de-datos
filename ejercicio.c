b3eb99e12d95ffebf8ac600d2c660c6fb1c510ec
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-21 09:53:01 -0300

    función crear lista, la cual genera una lista vacia

--- ejercicio.c
+++ ejercicio.c
@@ -6,7 +6,26 @@ struct nodo {
     struct nodo *sig;
 }typedef lista;
 
+lista *crearLista();
+void imprimirLista(lista *head);
 
 void main() {
+    lista *listaNumero = crearLista();
+    imprimirLista(listaNumero);
     printf("Hola mundo\n");
+}
+
+lista *crearLista() {
+    lista *head = (lista *)malloc(sizeof(lista));
+    head->dato = 0;
+    head->sig = NULL;
+    return head;
+}
+
+void imprimirLista(lista *head) {
+    lista *aux = head;
+    while (aux != NULL) {
+        printf("%d\n", aux->dato);
+        aux = aux->sig;
+    }
 }
