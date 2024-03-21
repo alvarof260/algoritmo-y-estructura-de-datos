743c71ccd1aa1e643821e0402f829c6dc857ab9d
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-21 09:58:49 -0300

    Agregada función para verificar si la lista está vacía

--- ejercicio.c
+++ ejercicio.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 
 struct nodo {
+    // Dato que almacena el nodo
     int dato;
+    // Apuntador a la estructura nodo
     struct nodo *sig;
 }typedef lista;
 
 lista *crearLista();
 void imprimirLista(lista *head);
+int esListaVacia(lista *e);
 
 void main() {
     lista *listaNumero = crearLista();
     imprimirLista(listaNumero);
+    esListaVacia(listaNumero) ? printf("La lista esta vacia\n") : printf("La lista no esta vacia\n");
     printf("Hola mundo\n");
 }
 
+// Función que crea una lista vacía
 lista *crearLista() {
     lista *head = (lista *)malloc(sizeof(lista));
     head->dato = 0;
@@ -22,10 +27,16 @@ lista *crearLista() {
     return head;
 }
 
+// Función que imprime los elementos de la lista
 void imprimirLista(lista *head) {
     lista *aux = head;
     while (aux != NULL) {
         printf("%d\n", aux->dato);
         aux = aux->sig;
     }
+}
+
+// Función que verifica si la lista está vacía
+int esListaVacia(lista *e) {
+    return e->dato == 0;
 }
