fe6a87b58983abb04aedfbb840246e31c7274b4b
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-04-02 12:11:34 -0300

    feat: agregar funciones de insertarK, longitud y pertenece

--- clase-practica-1/Lista.h
+++ clase-practica-1/Lista.h
@@ -44,6 +44,70 @@ Item primerElemento(Lista l)
     return l->dato;
 }
 
+void borrar(Lista *l)
+{
+    if (esListaVacia(*l))
+        return;
+    Nodo* actual = *l;
+    *l = (*l)->sig;
+    delete actual;
+}
+
+int longitud(Lista l)
+{
+    int i = 0;
+    while (l != NULL)
+    {
+        i++;
+        l = l->sig;
+    }
+    return i;
+}
+
+int pertenece(Lista l,Item dato)
+{
+    if (esListaVacia(l))
+        return 0;
+    while (l != NULL)
+    {
+        if(dato == l->dato)
+            return 1;
+        l = l->sig;
+    }
+    return 0;
+}
+
+void insertarK(Nodo** l, Item x, int k)
+{
+     if (k == 0)
+    {
+        insertar(l, x);
+        return;
+    }
+
+    Nodo* nuevo = new Nodo;
+    nuevo->dato = x;
+    nuevo->sig = nullptr; // Establecer el siguiente nodo del nuevo nodo como nullptr
+
+    Nodo* aux = *l;
+    int contador = 0;
+    while (aux != nullptr && contador < k - 1)
+    {
+        aux = aux->sig;
+        contador++;
+    }
+
+    if (aux == nullptr)
+    {
+        std::cerr << "La posición está más allá del final de la lista." << std::endl;
+        delete nuevo; // Liberar memoria del nuevo nodo antes de salir
+        return;
+    }
+
+    nuevo->sig = aux->sig;
+    aux->sig = nuevo;
+}
+
 void liberarMemoria(Lista *l)
 {
     Nodo* temp;
--- clase-practica-1/main.cpp
+++ clase-practica-1/main.cpp
@@ -12,7 +12,15 @@ int main()
     insertar(&head, 100);
     insertar(&head, 20);
     mostrar(head);
+    std::cout << "la longitud es: " << longitud(head)<< std::endl;
     std::cout << "el primer elemento es: " << primerElemento(head)<< std::endl;
+    borrar(&head);
+    pertenece(head, 50)
+        ? std::cout << "Pertenece\n" << std::endl
+        : std::cout << "No Pertenece\n" << std::endl;
+    insertarK(&head, 200, 1);
+    insertarK(&head, 250, 90);
+    mostrar(head);
     liberarMemoria(&head);
     return 0;
 }
